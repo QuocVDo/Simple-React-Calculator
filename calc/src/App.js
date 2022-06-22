1c57b5c770a32a46f8c77b5d0a17ac6ac9080cc5
QuocVDo <qdo@ucsd.edu>
2022-06-22 16:52:19 -0700

    Added useState hooks to update the display

--- calc/src/App.js
+++ calc/src/App.js
@@ -1,33 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calcgrid.css';
 
 function App() {
+  const [currDisplay, updateDisplay] = useState('');
+
+  //Function to handle normal button presses (numerical + operations)
+  function handleClick(value) {
+    updateDisplay((prevOut) => prevOut + value);
+  }
+
+  //Function to handle clearing the field
+  function handleClear() {
+    updateDisplay('');
+  }
+
+  //Function to handle deleting the last character in the field
+  function handleDel() {
+    updateDisplay((prevOut) => prevOut.slice(0, -1));
+  }
+
+  //Meat of the logic, handle the actual updating of the state logic
+  function handleEval() {
+    console.log(currDisplay);
+  }
   return (
     //Big Calculator Grid that encompasses all components
     <div className="calc-grid">
       <div className="output-box">
-        <div className="previousOp">123,456,789 * </div>
-        <div className="currOp">123,456,789</div>
+        <div className="previousOp">'placeholder'</div>
+        <div className="currOp">{currDisplay}</div>
       </div>
 
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="span-two">=</button>
+      <button onClick={handleClear} className="span-two">
+        AC
+      </button>
+      <button onClick={handleDel}>DEL</button>
+      <button onClick={() => handleClick('÷')}>÷</button>
+      <button onClick={() => handleClick('1')}>1</button>
+      <button onClick={() => handleClick('2')}>2</button>
+      <button onClick={() => handleClick('3')}>3</button>
+      <button onClick={() => handleClick('*')}>*</button>
+      <button onClick={() => handleClick('4')}>4</button>
+      <button onClick={() => handleClick('5')}>5</button>
+      <button onClick={() => handleClick('6')}>6</button>
+      <button onClick={() => handleClick('+')}>+</button>
+      <button onClick={() => handleClick('7')}>7</button>
+      <button onClick={() => handleClick('8')}>8</button>
+      <button onClick={() => handleClick('9')}>9</button>
+      <button onClick={() => handleClick('-')}>-</button>
+      <button onClick={() => handleClick('.')}>.</button>
+      <button onClick={() => handleClick('0')}>0</button>
+      <button onClick={handleEval} className="span-two">
+        =
+      </button>
     </div>
   );
 }

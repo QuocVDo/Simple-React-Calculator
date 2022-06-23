7f72923801355bba2a7a82f34fb6f3f118f6ebde
QuocVDo <qdo@ucsd.edu>
2022-06-22 18:24:30 -0700

    completed functionality, added title and styling

--- calc/src/App.js
+++ calc/src/App.js
@@ -3,10 +3,15 @@ import './calcgrid.css';
 
 function App() {
   const [currDisplay, updateDisplay] = useState('');
+  const [prevDisplay, updatePreviousDisplay] = useState('');
 
-  //Function to handle normal button presses (numerical + operations)
+  //Function to handle normal (numerical + operand) button presses
   function handleClick(value) {
-    updateDisplay((prevOut) => prevOut + value);
+    if (currDisplay === 'Syntax Error') {
+      updateDisplay(value);
+    } else {
+      updateDisplay((prevOut) => prevOut + value);
+    }
   }
 
   //Function to handle clearing the field
@@ -16,47 +21,67 @@ function App() {
 
   //Function to handle deleting the last character in the field
   function handleDel() {
-    updateDisplay((prevOut) => prevOut.slice(0, -1));
+    if (currDisplay === 'Syntax Error') {
+      updateDisplay('');
+    } else {
+      updateDisplay((prevOut) => prevOut.slice(0, -1));
+    }
   }
 
   //Meat of the logic, handle the actual updating of the state logic
   function handleEval() {
-    const operands = currDisplay.split('+');
-    console.log('Operand 1 is: ' + operands[0]);
-    console.log('Operand 2 is: ' + operands[1]);
-    updateDisplay(Number(operands[0]) + Number(operands[1]));
+    let result;
+
+    if (currDisplay === 'Syntax Error' || currDisplay === '') {
+      updateDisplay('');
+    } else {
+      try {
+        result = eval(currDisplay).toString();
+      } catch (error) {
+        result = 'Syntax Error';
+      }
+      updatePreviousDisplay(currDisplay);
+      updateDisplay(result);
+    }
   }
   return (
     //Big Calculator Grid that encompasses all components
-    <div className="calc-grid">
-      <div className="output-box">
-        <div className="previousOp">'placeholder'</div>
-        <div className="currOp">{currDisplay}</div>
+    <>
+      <div className="title">
+        <h1>Simple React Calculator</h1>
+        <h2>By: Quoc-Zuy Do</h2>
       </div>
 
-      <button onClick={handleClear} className="span-two">
-        AC
-      </button>
-      <button onClick={handleDel}>DEL</button>
-      <button onClick={() => handleClick('÷')}>÷</button>
-      <button onClick={() => handleClick('1')}>1</button>
-      <button onClick={() => handleClick('2')}>2</button>
-      <button onClick={() => handleClick('3')}>3</button>
-      <button onClick={() => handleClick('*')}>*</button>
-      <button onClick={() => handleClick('4')}>4</button>
-      <button onClick={() => handleClick('5')}>5</button>
-      <button onClick={() => handleClick('6')}>6</button>
-      <button onClick={() => handleClick('+')}>+</button>
-      <button onClick={() => handleClick('7')}>7</button>
-      <button onClick={() => handleClick('8')}>8</button>
-      <button onClick={() => handleClick('9')}>9</button>
-      <button onClick={() => handleClick('-')}>-</button>
-      <button onClick={() => handleClick('.')}>.</button>
-      <button onClick={() => handleClick('0')}>0</button>
-      <button onClick={handleEval} className="span-two">
-        =
-      </button>
-    </div>
+      <div className="calc-grid">
+        <div className="output-box">
+          <div className="previousOp">{prevDisplay}</div>
+          <div className="currOp">{currDisplay}</div>
+        </div>
+
+        <button onClick={handleClear} className="span-two">
+          AC
+        </button>
+        <button onClick={handleDel}>DEL</button>
+        <button onClick={() => handleClick('/')}>/</button>
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('.')}>.</button>
+        <button onClick={() => handleClick(0)}>0</button>
+        <button onClick={handleEval} className="span-two">
+          =
+        </button>
+      </div>
+    </>
   );
 }
 
